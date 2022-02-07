5118f489c379c377e75235d979a7fa79e5f4e62c
Tova0811 <tova.folkesson@gmail.com>
2022-02-07 11:12:58 +0100

    week-three

---
+++ week-three/class-one/bootstrap/init.php
@@ -0,0 +1,10 @@
+<?php
+
+define('ROOT_PATH', realpath(dirname(__FILE__) . '/../'));
+
+require(ROOT_PATH . '/vendor/autoload.php/');
+
+require (ROOT_PATH . '/config/app.php');
+
+require (ROOT_PATH . '/bootstrap/db.php');
+
